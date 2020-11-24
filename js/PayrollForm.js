7e9050e1656ae9e1628db1bb8f9aa427b930e8e1
NCSaketh <nc.saketh1@gmail.com>
2020-11-24 22:30:23 +0530

    Local Storage UC2 : Ser EventListeners when doc is loaded

--- js/PayrollForm.js
+++ js/PayrollForm.js
@@ -1,11 +1,26 @@
-//UC8
+//UC12
 
-const salary = document.querySelector('#salary')
-const output = document.querySelector('.salary-output')
-output.textContent = salary.value;
-salary.addEventListener('input', function () {
+window.addEventListener('DOMContentLoaded', (event) => {
+    const name = document.querySelector('#name');
+    const nameError = document.querySelector('.name-error');
+    name.addEventListener('input', function() {
+        if(name.value.length == 0) {
+            nameError.textContent = "";
+            return;
+        }
+        try {
+            (new EmployeePayrollData()).name = name.value;
+            nameError.textContent = "";
+        } catch (e) {
+            nameError.textContent = e;
+        }
+    });
+    const salary = document.querySelector('#salary')
+    const output = document.querySelector('.salary-output')
     output.textContent = salary.value;
-});
+    salary.addEventListener('input', function () {
+        output.textContent = salary.value;
+    });
 
 //UC9 , UC10, UC11
 
